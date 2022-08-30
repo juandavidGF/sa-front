c2524d74d24e50ec70a24249280fe7365a69573f
juandavidGF <davad701@gmail.com>
2022-08-29 20:52:12 -0500

    text is not defined

--- api/predict.js
+++ api/predict.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
 		try {
 			// const response = await axios.get(uri);
 			// const text = JSON.parse(req.body);
-			res.status(200).json(text);
+			res.status(200).json(req.body);
 		} catch (e) {
 			console.log(e);
 			res.status(500).json(e);
