2aba19696139736a0db4fb765595a50fbcc6165b
juandavidGF <davad701@gmail.com>
2022-08-29 20:30:32 -0500

    log text

--- api/predict.js
+++ api/predict.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
 	if (req.method === 'POST') {
 		try {
 			// const response = await axios.get(uri);
-			res.status(200).json(req.body);
+			res.status(200).json(req.body.text);
 		} catch (error) {
 			console.log(e);
 			res.status(500).json(e);
