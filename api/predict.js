dffe775947215f1c8ef3c67728047b8c0e979666
juandavidGF <davad701@gmail.com>
2022-08-29 20:49:04 -0500

    fix typo

--- api/predict.js
+++ api/predict.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
 			// const response = await axios.get(uri);
 			// const text = JSON.parse(req.body);
 			res.status(200).json(text);
-		} catch (error) {
+		} catch (e) {
 			console.log(e);
 			res.status(500).json(e);
 		}
