3851191af6732b88d743953e6c0bfe43d2262445
juandavidGF <davad701@gmail.com>
2022-08-29 21:15:46 -0500

    add httos module and call the endpoint

--- api/predict.js
+++ api/predict.js
@@ -1,8 +1,12 @@
-import axios from 'axios';
+const https = require('https');
 
 export default async function handler(req, res) {
 	if (req.method === 'POST') {
 		try {
+			https.get("http://3.234.151.75:5555/predict?msg=", (resp) => {
+				console.log(resp)
+
+			})
 			// const response = await axios.get(uri);
 			// const text = JSON.parse(req.body);
 			// var response = {
@@ -12,7 +16,7 @@ export default async function handler(req, res) {
 			// }
 			var msj = req.body.text
 
-			console.log(req.body.text)
+			// console.log(req.body.text)
 			res.status(200).json(req.body.text);
 		} catch (e) {
 			console.log(e);
